9d3c736e6555f313d9b3886c34c41af5fdb2f7d0
Jacob Maier <jacobamaier@gmail.com>
2023-02-07 14:38:46 -0800

    push to test on another device.

--- src/index.js
+++ src/index.js
@@ -15,25 +15,33 @@ function searchGiphy(search) {
   );
 }
 
-const printError = (error) => {
+//start here
+function printElements(resource) {
+  document.getElementById("showResults").innerHTML = null;
+  document.getElementById("keyword").value = null;
+
+  if (resource.data[1] !== undefined) {
+    resource.data.forEach((gif) => {
+      const img = document.createElement("img");
+      img.setAttribute("src", gif.images.original.url);
+      img.setAttribute("width", "150px");
+      document.getElementById("showResults").append(img);
+    });
+  }
+}
+
+function printError(error) {
   document.querySelector(
     "#showResults"
   ).innerText = `${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
-};
-//start here
-const printElements = (data) => {
-  document.getElementById("showResults").innerHTML = null;
-  document.getElementById("keyword").value;
-  const img = document.createElement("img");
-  data.data.forEach((gif) => {
-    img.setAttribute("src", gif.images.url);
-    document.getElementById("showResults").append(img);
-  });
-};
+}
 
 window.addEventListener("load", () => {
-  document.getElementById("searchForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-    searchGiphy();
-  });
+  document
+    .getElementById("searchForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      let search = document.getElementById("keyword").value;
+      searchGiphy(search);
+    });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@ export class GiphyService {
   static searchGiphy(search) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=25&offset=0&rating=g&lang=en`;
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=20&offset=0&rating=pg&lang=en`;
 
       request.addEventListener("loadend", function () {
-        const response = JSON.parse(this.responseText);
+        let response = JSON.parse(this.responseText);
         if (this.status === 200) {
-          resolve([response, search]);
+          resolve(response);
         } else {
           reject([this, response, search]);
         }
