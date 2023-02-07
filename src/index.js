3047a16aa4404c7064acbb0db2a2ff630725987c
Jacob Maier <jacobamaier@gmail.com>
2023-02-06 20:38:08 -0800

    WIP displaying giphy gifs.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import { GiphyAPI } from './../src/js';
+import { GiphyService } from "./js/main";
 
-  function printElements(response) {
-    let results = document.getElementById('searchResults');
-    let response = 
-  }
+function searchGiphy(userSearchQuery) {
+  let promise = GiphyService.showGiphy(userSearchQuery);
+  promise.then(function() {
+    printElement(giphyData);
+  }, function(giphyErrorArray) {
+    printElement(giphyErrorArray);
+  });
+}
 
+function printElement() {
+  document.getElementById('ShowResults').innerHTML = null;
+  document.getElementById('keyword').value;
+}
+
+function handleForm() {
+  let searchKeyword = document.getElementById("keyword").value;
+}
+
+window.addEventListener('load', function() {
+  event.preventDefault();
+  document.getElementById('searchForm').addEventListener('submit', handleForm);
+  searchGiphy();
+  printElement();
+});
